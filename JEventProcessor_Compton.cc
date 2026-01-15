634d711388346d448143647cdec8320f2cabb887
hatdaq <hatdaq@comptondaq.jlab.org>
2026-01-15 14:23:48 -0500

    New update for Compton analyzer, add the fadc250 pulse tree to the output root file

--- JEventProcessor_Compton.cc
+++ JEventProcessor_Compton.cc
@@ -15,8 +15,7 @@ JEventProcessor_Compton::JEventProcessor_Compton() {
     m_fadc_scaler_hits_in.SetOptional(true);
     m_pulse_hits_in.SetOptional(true);
     m_waveform_hits_in.SetOptional(true);
-    m_ti_scaler_hits_in.SetOptional(true);
-    m_heldec_data_in.SetOptional(true);
+    m_it_scaler_hits_in.SetOptional(true);
 }
 
 /**
@@ -43,32 +42,17 @@ void JEventProcessor_Compton::Init() {
     m_waveform_tree->Branch("chan", &ev_chan);
     m_waveform_tree->Branch("waveform", &ev_waveform);
 
-    // Create the Helicity Decoder Tree
-    m_tree = new TTree("m_tree", "Physics Event Tree");
-    m_tree->Branch(
-        "heldec",
-        &heldec,
-        "helicity_seed/i:"
-        "n_tstable_fall/i:"
-        "n_tstable_rise/i:"
-        "n_pattsync/i:"
-        "n_pairsync/i:"
-        "time_tstable_start/i:"
-        "time_tstable_end/i:"
-        "last_tstable_duration/i:"
-        "last_tsettle_duration/i:"
-        "trig_tstable/i:"
-        "trig_pattsync/i:"
-        "trig_pairsync/i:"
-        "trig_helicity/i:"
-        "trig_pat0_helicity/i:"
-        "trig_polarity/i:"
-        "trig_pat_count/i:"
-        "last32wins_pattsync/i:"
-        "last32wins_pairsync/i:"
-        "last32wins_helicity/i:"
-        "last32wins_pattsync_hel/i"
-    );
+    m_pulse_tree = new TTree("pulse_tree","FADC250 pulse data(slow, channel, integral, time)");
+    m_pulse_tree->Branch("integral_sum", &ev_integral_sum);
+    m_pulse_tree->Branch("pedestal_sum", &pedestal_sum);
+    m_pulse_tree->Branch("coarse_time",&ev_coarse_time);
+    m_pulse_tree->Branch("fine_time",&ev_fine_time);
+    m_pulse_tree->Branch("pulse_peak",&ev_pulse_peak);
+    m_pulse_tree->Branch("pedestal_quality",&pedestal_quality);
+    m_pulse_tree->Branch("nhits",&number_hit);
+    m_pulse_tree->Branch("chan",&ev_pulse_chan);
+    m_pulse_tree->Branch("slot",&ev_pulse_slot);
+
 
     // Optionally: Text output file for human-readable hit summaries
     m_txt_output_file.open(m_txt_output_filename().c_str());
@@ -92,6 +76,15 @@ void JEventProcessor_Compton::ProcessSequential(const JEvent &event) {
     ev_slot.clear();
     ev_chan.clear();
     ev_waveform.clear();
+    ev_coarse_time.clear();
+    ev_pulse_chan.clear();
+    ev_pulse_slot.clear();
+    ev_fine_time.clear();
+    ev_integral_sum.clear();
+    ev_pulse_peak.clear();
+    pedestal_sum= 0;
+    pedestal_quality = 0;
+    number_hit =0;
 
     // FADC250 waveform hits
     for (const auto& waveform_hit : m_waveform_hits_in()) {
@@ -106,38 +99,39 @@ void JEventProcessor_Compton::ProcessSequential(const JEvent &event) {
 	ev_chan.insert(ev_chan.end(), waveform_sample_number, m_waveform_tree_row.chan);
 	ev_waveform.insert(ev_waveform.end(),  m_waveform_tree_row.waveform.begin(), m_waveform_tree_row.waveform.end());
     }
+    
+    // FADC250 pulse hits
+ 
+    int nn=0;
+    for (const auto& pulse_hit : m_pulse_hits_in()){
+        integral_sum = pulse_hit->integral_sum;
+        pedestal_sum = pulse_hit->pedestal_sum;
+        coarse_time = pulse_hit->coarse_time;
+        fine_time = pulse_hit->fine_time;
+        pulse_peak = pulse_hit->pulse_peak;
+        pedestal_quality = pulse_hit->pedestal_quality;
+        if(integral_sum!=0){
+            nn++;
+            ev_integral_sum.push_back(integral_sum);
+            ev_coarse_time.push_back(coarse_time);
+            ev_fine_time.push_back(fine_time);
+            ev_pulse_peak.push_back(pulse_peak);
+            ev_pulse_slot.push_back(pulse_hit->slot);
+            ev_pulse_chan.push_back(pulse_hit->chan);
+        }
+       
+        
+    }
+    number_hit = nn;
     m_waveform_tree->Fill();
+    if(nn>0){
+        m_pulse_tree->Fill();
+    }
 
     // FADC250 pulse hits
     for (const auto& pulse_hit : m_pulse_hits_in()) {
         // Fill histogram with pulse integral values
         m_pulse_integral_hist->Fill(pulse_hit->integral_sum);
-    }
-
-    heldec = {};
-    // Helicity decoder data
-    for(const auto& heldec_hit : m_heldec_data_in()){
-	heldec.helicity_seed          = heldec_hit->helicity_seed;
-        heldec.n_tstable_fall         = heldec_hit->n_tstable_fall;
-        heldec.n_tstable_rise         = heldec_hit->n_tstable_rise;
-        heldec.n_pattsync             = heldec_hit->n_pattsync;
-        heldec.n_pairsync             = heldec_hit->n_pairsync;
-        heldec.time_tstable_start     = heldec_hit->time_tstable_start;
-        heldec.time_tstable_end       = heldec_hit->time_tstable_end;
-        heldec.last_tstable_duration  = heldec_hit->last_tstable_duration;
-        heldec.last_tsettle_duration  = heldec_hit->last_tsettle_duration;
-        heldec.trig_tstable           = heldec_hit->trig_tstable;
-        heldec.trig_pattsync          = heldec_hit->trig_pattsync;
-        heldec.trig_pairsync          = heldec_hit->trig_pairsync;
-        heldec.trig_helicity          = heldec_hit->trig_helicity;
-        heldec.trig_pat0_helicity     = heldec_hit->trig_pat0_helicity;
-        heldec.trig_polarity          = heldec_hit->trig_polarity;
-        heldec.trig_pat_count         = heldec_hit->trig_pat_count;
-        heldec.last32wins_pattsync    = heldec_hit->last32wins_pattsync;
-        heldec.last32wins_pairsync    = heldec_hit->last32wins_pairsync;
-        heldec.last32wins_helicity    = heldec_hit->last32wins_helicity;
-        heldec.last32wins_pattsync_hel= heldec_hit->last32wins_pattsync_hel;
-        m_tree->Fill();
     }
 
     // ------------------------------------------------------------------
@@ -147,15 +141,15 @@ void JEventProcessor_Compton::ProcessSequential(const JEvent &event) {
         const auto& waveform_hits      = m_waveform_hits_in();
         const auto& pulse_hits         = m_pulse_hits_in();
         const auto& fadc_scaler_hits   = m_fadc_scaler_hits_in();
-        const auto& ti_scaler_hits     = m_ti_scaler_hits_in();
+        const auto& it_scaler_hits     = m_it_scaler_hits_in();
 
         bool have_waveforms       = !waveform_hits.empty();
         bool have_pulses          = !pulse_hits.empty();
         bool have_fadc_scalers    = !fadc_scaler_hits.empty();
-        bool have_ti_scalers      = !ti_scaler_hits.empty();
+        bool have_it_scalers      = !it_scaler_hits.empty();
 
         // Only write anything if we have at least one type of hit
-        if (have_waveforms || have_pulses || have_fadc_scalers || have_ti_scalers) {
+        if (have_waveforms || have_pulses || have_fadc_scalers || have_it_scalers) {
             auto event_number = event.GetEventNumber();
 
             m_txt_output_file << "Event " << event_number << "\n";
@@ -209,12 +203,12 @@ void JEventProcessor_Compton::ProcessSequential(const JEvent &event) {
                 m_txt_output_file << "  No FADCScalerHit objects in this event\n";
             }
 
-            // TI scaler summary
-            if (have_ti_scalers) {
-                m_txt_output_file << "  TI scaler hits: " << ti_scaler_hits.size() << "\n";
-                for (const auto& hit : ti_scaler_hits) {
+            // IT scaler summary
+            if (have_it_scalers) {
+                m_txt_output_file << "  IT scaler hits: " << it_scaler_hits.size() << "\n";
+                for (const auto& hit : it_scaler_hits) {
                     m_txt_output_file
-                        << "    TISCALER rocid=" << hit->rocid
+                        << "    ITSCALER rocid=" << hit->rocid
                         << " slot=" << hit->slot
                         << " nwords=" << hit->nscalerwords
                         << " live_time=" << hit->live_time
@@ -223,7 +217,7 @@ void JEventProcessor_Compton::ProcessSequential(const JEvent &event) {
                         << "\n";
                 }
             } else {
-                m_txt_output_file << "  No TIScalerHit objects in this event\n";
+                m_txt_output_file << "  No ITScalerHit objects in this event\n";
             }
 
             m_txt_output_file << "\n";
@@ -244,7 +238,7 @@ void JEventProcessor_Compton::Finish() {
     if (m_root_output_file) {
         m_waveform_tree->Write();        // Save waveform tree to file
         m_pulse_integral_hist->Write();  // Save integral histogram to file
-	m_tree->Write();
+        m_pulse_tree->Write();
         m_root_output_file->Close();     // Close ROOT file
         delete m_root_output_file;       // Free memory
         m_root_output_file = nullptr;
--- JEventProcessor_Compton.h
+++ JEventProcessor_Compton.h
@@ -13,8 +13,7 @@
 #include <JANA/JEventProcessor.h>
 #include "EventHits_FADC.h"
 #include "FADCScalerHit.h"
-#include "TIScalerHit.h"
-#include "HelicityDecoderData.h"
+#include "ITScalerHit.h"
 
 /**
  * @struct WaveformTreeRow
@@ -31,28 +30,6 @@ struct WaveformTreeRow {
     std::vector<uint32_t> waveform;
 };
 
-struct HelDec_t {
-    uint32_t helicity_seed;
-    uint32_t n_tstable_fall;
-    uint32_t n_tstable_rise;
-    uint32_t n_pattsync;
-    uint32_t n_pairsync;
-    uint32_t time_tstable_start;
-    uint32_t time_tstable_end;
-    uint32_t last_tstable_duration;
-    uint32_t last_tsettle_duration;
-    uint32_t trig_tstable;
-    uint32_t trig_pattsync;
-    uint32_t trig_pairsync;
-    uint32_t trig_helicity;
-    uint32_t trig_pat0_helicity;
-    uint32_t trig_polarity;
-    uint32_t trig_pat_count;
-    uint32_t last32wins_pattsync;
-    uint32_t last32wins_pairsync;
-    uint32_t last32wins_helicity;
-    uint32_t last32wins_pattsync_hel;
-};
 
 /**
  * @class JEventProcessor_Compton
@@ -72,8 +49,7 @@ private:
     Input<FADC250WaveformHit> m_waveform_hits_in {this}; 
     Input<FADC250PulseHit>    m_pulse_hits_in {this};
     Input<FADCScalerHit>      m_fadc_scaler_hits_in {this};
-    Input<TIScalerHit>        m_ti_scaler_hits_in {this};
-    Input<HelicityDecoderData>  m_heldec_data_in {this};
+    Input<ITScalerHit>        m_it_scaler_hits_in {this};
 
     /**
      * @brief ROOT output filename parameter
@@ -101,9 +77,21 @@ private:
     std::vector<uint32_t> ev_slot;
     std::vector<uint32_t> ev_chan;
     std::vector<uint32_t> ev_waveform;
+    uint32_t integral_sum;
+    uint32_t coarse_time;
+    uint32_t fine_time;
+    uint32_t pulse_peak;
+    uint32_t pedestal_sum;
+    uint32_t pedestal_quality;
+    int number_hit;
+    std::vector<uint32_t> ev_integral_sum;
+    std::vector<uint32_t> ev_coarse_time;
+    std::vector<uint32_t> ev_fine_time;
+    std::vector<uint32_t> ev_pulse_peak;
+    std::vector<uint32_t> ev_pulse_slot;
+    std::vector<uint32_t> ev_pulse_chan;
 
-    // helicity decoder tree variables
-    HelDec_t heldec{};
+
 
     // ROOT output objects
     TFile *m_root_output_file;                ///< ROOT file for histogram and tree storage
@@ -111,6 +99,9 @@ private:
     TTree *m_waveform_tree;                   ///< ROOT tree for waveform data
     TTree *m_tree;                            ///< ROOT tree for physics event
     TH1I *m_pulse_integral_hist;              ///< Histogram of pulse integral sums
+
+    TTree *m_pulse_tree;                      ///< ROOT tree for pulse hit
+
 
     // Text output for human-readable dump of hits per event
     std::ofstream m_txt_output_file;
