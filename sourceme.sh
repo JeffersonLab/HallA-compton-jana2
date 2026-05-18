7e3b068f5027782267b6761257a31cfd53232ed2
hanjie1 <hanjie@jlab.org>
2026-05-18 10:52:26 -0400

    local changes

--- sourceme.sh
+++ sourceme.sh
@@ -1,3 +1,4 @@
 export JCE_CONFIG_DIR=$PWD/config
-export CMAKE_PREFIX_PATH="/adaqfs/home/acompton/software/JANA2:/adaqfs/home/acompton/software/jana2-common-extensions/install":$CMAKE_PREFIX_PATH
+export CMAKE_PREFIX_PATH="/w/halla-scshelf2102/solid/hanjie/JANA2:/w/halla-scshelf2102/solid/hanjie/jana2-common-extensions/install":$CMAKE_PREFIX_PATH
+export JANA_PLUGIN_PATH=$JCE_HOME/lib/plugins:$PWD/install/lib/plugins
 #cmake --install build --prefix=install
